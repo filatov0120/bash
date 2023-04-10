eba168d8f7407770e52534e67ba7927c07dc8bf9
Alexander <128065248+filatov0120@users.noreply.github.com>
2023-04-10 22:37:32 +0300

    Create mongo_backup.sh

---
+++ mongo_backup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Backup MongoDB databases
+# Remove backups that are older than 3 days
+
+# Set backup directory and file name
+BACKUP_DIR="/data/backup_loop_mongo"
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+BACKUP_NAME="mongodb_backup_$DATE"
+
+# Run mongodump command to create backup
+mongodump --authenticationDatabase=admin --username= --password= --db=admin --out $BACKUP_DIR/$BACKUP_NAME --gzip
+
+# Set the number of backups to keep
+num_backups_to_keep=3
+
+# Remove backups more than 3
+cd $BACKUP_DIR
+ls -1t | grep "^mongodb_backup_" | tail -n +4 | xargs -d '\n' rm -R -f
