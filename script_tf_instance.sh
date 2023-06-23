49f4e4f644de343577a248eb5f085d68680376f1
Alexander <128065248+filatov0120@users.noreply.github.com>
2023-06-23 23:20:57 +0300

    Update and rename script_tf_instance to script_tf_instance.sh

--- script_tf_instance.sh
+++ script_tf_instance.sh
@@ -10,6 +10,10 @@ echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docke
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#Create dir and mount EBS
 sudo mkdir /data
 sudo mkfs -t xfs /dev/nvme1n1
 sudo mount /dev/nvme1n1 /data
+#Add auto mount
+ebs_uuid=$(sudo blkid -s UUID -o value /dev/nvme1n1)
+echo "UUID=$ebs_uuid  /data  xfs  defaults,nofail  0  2" | sudo tee -a /etc/fstab
