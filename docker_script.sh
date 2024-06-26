1924d406ac498c2efb2055f605a8c65e58f2e445
Alexander Filatov <128065248+filatov0120@users.noreply.github.com>
2024-06-26 14:10:14 +0300

    Update docker_script.sh

--- docker_script.sh
+++ docker_script.sh
@@ -11,7 +11,7 @@ echo \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #Append User to docker group 
 sudo usermod -aG docker $USER
 #Reboot instance
